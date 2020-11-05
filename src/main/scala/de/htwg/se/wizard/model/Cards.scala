cd53d37aa51e19457cf7f299c7421344307c4c3b
Niclas Strenger <n.strenger1@gmail.com>
2020-11-05 19:34:43 +0100

    Added all Cards

--- src/main/scala/de/htwg/se/wizard/model/Cards.scala
+++ src/main/scala/de/htwg/se/wizard/model/Cards.scala
@@ -1,8 +1,75 @@
 package de.htwg.se.wizard.model
 
 case object Cards {
-    val all_cards = Array(
+    val all_cards: Array[Card] = Array(
       Card(0, "green"),
-      Card(1, "green")
+      Card(0, "white"),
+      Card(0, "blue"),
+      Card(0, "red"),
+
+      Card(1, "green"),
+      Card(1, "white"),
+      Card(1, "blue"),
+      Card(1, "red"),
+
+      Card(2, "green"),
+      Card(2, "white"),
+      Card(2, "blue"),
+      Card(2, "red"),
+
+      Card(3, "green"),
+      Card(3, "white"),
+      Card(3, "blue"),
+      Card(3, "red"),
+
+      Card(4, "green"),
+      Card(4, "white"),
+      Card(4, "blue"),
+      Card(4, "red"),
+
+      Card(5, "green"),
+      Card(5, "white"),
+      Card(5, "blue"),
+      Card(5, "red"),
+
+      Card(6, "green"),
+      Card(6, "white"),
+      Card(6, "blue"),
+      Card(6, "red"),
+
+      Card(7, "green"),
+      Card(7, "white"),
+      Card(7, "blue"),
+      Card(7, "red"),
+
+      Card(8, "green"),
+      Card(8, "white"),
+      Card(8, "blue"),
+      Card(8, "red"),
+
+      Card(9, "green"),
+      Card(9, "white"),
+      Card(9, "blue"),
+      Card(9, "red"),
+
+      Card(10, "green"),
+      Card(10, "white"),
+      Card(10, "blue"),
+      Card(10, "red"),
+
+      Card(11, "green"),
+      Card(11, "white"),
+      Card(11, "blue"),
+      Card(11, "red"),
+
+      Card(12, "green"),
+      Card(12, "white"),
+      Card(12, "blue"),
+      Card(12, "red"),
+
+      Card(13, "green"),
+      Card(13, "white"),
+      Card(13, "blue"),
+      Card(13, "red"),
     )
 }
