aacfd330e34f71afb43a2d1ce9f11fa462cb29f4
Niclas Strenger <n.strenger1@gmail.com>
2020-11-11 15:54:53 +0100

    requirements of Gamestate added

--- src/main/scala/de/htwg/se/wizard/aview/TUI.scala
+++ src/main/scala/de/htwg/se/wizard/aview/TUI.scala
@@ -1,7 +1,15 @@
 package de.htwg.se.wizard.aview
 
+import de.htwg.se.wizard.model.Card
+
 class TUI {
     def processInput(input : String) : Wizard = {
 
+    }
+    def nextplayerCard():Card={
+        //"Spieler x wähle deine karte"
+        //zeigt Spieler seine Karten
+        //isPlayable(Karte)
+        //PlayCard
     }
 }
