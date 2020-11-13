aa222cef8de7ac6d5855c66947a03a8be6de83ac
Niclas Strenger <n.strenger1@gmail.com>
2020-11-13 07:41:58 +0100

    changed import

--- src/main/scala/de/htwg/se/wizard/model/Random.scala
+++ src/main/scala/de/htwg/se/wizard/model/Random.scala
@@ -1,14 +1,12 @@
 package de.htwg.se.wizard.model
 
-import scala.util.Random
-
 trait Random {
   def generateHand(round: Int, used : List[Card] = List[Card]()) : (List[Card], List[Card]) = {
     var handCards = List[Card]().empty
     var usedCards = used
     var nextCard = Cards.all_cards(0)
     for (_ <-0.until(round)){
-      nextCard = (Cards.all_cards diff usedCards)(Random.between(0,60-usedCards.size))
+      nextCard = (Cards.all_cards diff usedCards)(scala.util.Random.between(0,60-usedCards.size))
       handCards = nextCard :: handCards
       usedCards = nextCard :: usedCards
     }
