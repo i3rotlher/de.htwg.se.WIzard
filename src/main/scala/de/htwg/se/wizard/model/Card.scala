5856bd8c83178dc4bafbc47b2fcc91dd01bc7eb3
Niclas Strenger <n.strenger1@gmail.com>
2020-12-03 16:35:11 +0100

    Pattern implemented and tests adjusted

--- src/main/scala/de/htwg/se/wizard/model/Card.scala
+++ src/main/scala/de/htwg/se/wizard/model/Card.scala
@@ -5,12 +5,12 @@ abstract case class Card(num: Int, colour: String) {
   override def toString():String = get_String
 }
 class Card_with_value(num: Int, colour: String) extends Card(num, colour) {
-    override def get_String: String = "%s, %s".format(num.toString, colour)
+    override def get_String: String = "(%s, %s)".format(num.toString, colour)
   }
 class Card_wizard(num: Int, colour: String) extends Card(num, colour) {
-    override def get_String: String = "%s".format("Wizard")
+    override def get_String: String = "(%s, %s)".format("Zauberer", colour)
   }
 class Card_fool(num: Int, colour:String) extends Card(num, colour){
-    override def get_String: String = "%s".format("Fool")
+    override def get_String: String = "(%s, %s)".format("Narr", colour)
   }
 
